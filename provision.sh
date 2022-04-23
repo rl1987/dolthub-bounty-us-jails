821846cfbc3188b06ce55f3375d29bab0d6c9f69
rl1987 <rl1987@users.noreply.github.com>
2022-04-23 16:27:43 +0300

    Add terraform configuration with provisioning script

---
+++ provision.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y python3 python3-pip tmux git vim visidata
+curl -L https://github.com/dolthub/dolt/releases/latest/download/install.sh > /tmp/install.sh && bash /tmp/install.sh
+dolt config --global --add user.email rimantas@keyspace.lt
+dolt config --global --add user.name "rl1987"
+pip3 install openpyxl requests lxml js2xml doltpy xlrd
+
+curl -sSL https://repos.insights.digitalocean.com/install.sh -o /tmp/install.sh
+bash /tmp/install.sh
