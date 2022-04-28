660b9fbfd5639c27f228ea72a582791a2dd8b7c4
rl1987 <rl1987@users.noreply.github.com>
2022-04-28 23:24:01 +0300

    Install golang with bounty payment scoring program

--- provision.sh
+++ provision.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 apt-get update
 apt-get install -y python3 python3-pip tmux git vim visidata
 curl -L https://github.com/dolthub/dolt/releases/latest/download/install.sh > /tmp/install.sh && bash /tmp/install.sh
@@ -9,3 +11,26 @@ pip3 install openpyxl requests lxml js2xml doltpy xlrd TableauScraper PyPDF2 tab
 
 curl -sSL https://repos.insights.digitalocean.com/install.sh -o /tmp/install.sh
 bash /tmp/install.sh
+
+mkdir /root/go
+
+wget https://golang.org/dl/go1.18.1.linux-amd64.tar.gz -O /tmp/go1.18.1.linux-amd64.tar.gz
+tar -C /usr/local -xzf /tmp/go1.18.1.linux-amd64.tar.gz
+echo "export PATH=\$PATH:/usr/local/go/bin" >> /etc/profile
+echo "export GOPATH=/root/go" >> /etc/profile
+
+pushd /bin
+ln -s /usr/local/go/bin/go go
+popd
+
+pushd /tmp
+git clone https://github.com/dolthub/bounties.git
+pushd /tmp/bounties/go/payments/cmd/calc_payments
+go install .
+popd
+popd
+
+pushd /bin
+ln -s /root/go/bin/calc_payments calc_payments
+popd
+
