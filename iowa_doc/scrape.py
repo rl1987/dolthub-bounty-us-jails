81c3ab28ab19f47aa4b29bfaf2250aa06cf72ce3
rl1987 <rl1987@users.noreply.github.com>
2022-06-06 20:06:42 +0200

    Try to scrape Iowa DOC monthly snapshots

--- iowa_doc/scrape.py
+++ iowa_doc/scrape.py
@@ -1,12 +1,96 @@
 #!/usr/bin/python3
 
+import csv
+from pprint import pprint
+
+from dateutil.parser import parse
 import requests
 import pandas as pd
+from lxml import html
 
 def scrape_snapshot(url):
     print(url)
 
-    # TODO
+    resp = requests.get(url)
+
+    tree = html.fromstring(resp.text)
+
+    snapshot_date = tree.xpath("//h2/text()")[-1]
+    snapshot_date = parse(snapshot_date)
+    snapshot_date = snapshot_date.isoformat().split("T")[0]
+
+    dfs = pd.read_html(url)
+    if len(dfs) == 3:
+        df = dfs[0]
+    else:
+        df = dfs[1]
+    df = df[:-2]
+    df.set_index('Institution', inplace=True)
+    df.drop(columns=['Capacity', 'Medical/Segregation'], inplace=True)
+
+    print(df)
+
+    rows = []
+
+    rows.append({
+        'id': "IA_ANAMOSA",
+        'snapshot_date': snapshot_date,
+        'total': int(df.loc['Anamosa']['Current Count'].replace(",", "")),
+        'source_url': url
+    })
+
+    rows.append({
+        'id': "IA_CLARINDA",
+        'snapshot_date': snapshot_date,
+        'total': int(df.loc['Clarinda']['Current Count'].replace(",", "")),
+        'source_url': url
+    })
+
+    rows.append({
+        'id': "IA_FORTDODGE",
+        'snapshot_date': snapshot_date,
+        'total': int(df.loc['Fort Dodge']['Current Count'].replace(",", "")),
+        'source_url': url
+    })
+
+    rows.append({
+        'id': "IA_MTPLEASANT",
+        'snapshot_date': snapshot_date,
+        'total': int(df.loc['Mount Pleasant']['Current Count'].replace(",", "")),
+        'source_url': url
+    })
+
+    rows.append({
+        'id': "IA_NEWTON",
+        'snapshot_date': snapshot_date,
+        'total': int(df.loc['Newton-Medium']['Current Count'].replace(",", "")) + int(df.loc['Minimum']['Current Count'].replace(",", "")),
+        'source_url': url
+    })
+
+    rows.append({
+        'id': "IA_ROCKWELL",
+        'snapshot_date': snapshot_date,
+        'total': int(df.loc['Rockwell City']['Current Count'].replace(",", "")),
+        'source_url': url
+    })
+
+    rows.append({
+        'id': "IA_STATE",
+        'snapshot_date': snapshot_date,
+        'total': int(df.loc['Fort Madison']['Current Count'].replace(",", "")),
+        'source_url': url
+    })
+
+    rows.append({
+        'id': "IA_WOMEN",
+        'snapshot_date': snapshot_date,
+        'total': int(df.loc['Mitchellville']['Current Count'].replace(",", "")) + int(df.loc['Minimum Live-Out']['Current Count'].replace(",", "")),
+        'source_url': url
+    })
+
+    pprint(rows)
+
+    return rows
 
 def try_scraping_at_month(year, month):
     print(year, month)
@@ -39,9 +123,23 @@ def try_scraping_at_month(year, month):
     return scrape_snapshot(url)
 
 def main():
+    out_f = open("inmate_population_snapshots.csv", "w", encoding="utf-8")
+
+    csv_writer = csv.DictWriter(out_f, fieldnames=["id", "snapshot_date", "total", "source_url"], lineterminator="\n")
+    csv_writer.writeheader()
+
     for year in range(2017, 2023):
         for month in range(1, 13):
-            try_scraping_at_month(year, month)
+            rows = try_scraping_at_month(year, month)
+            
+            if rows is None:
+                continue
+
+            for row in rows:
+                csv_writer.writerow(row)
+
+    out_f.close()
+
 
 if __name__ == "__main__":
     main()
